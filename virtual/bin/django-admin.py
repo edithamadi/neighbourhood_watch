87306448fb440dea971a63a8616b72ef964e62a0
Edith <you@example.com>
2018-10-19 19:25:12 +0300

    installed dependencies

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/edith/Documents/core/django/django_projects/neighbourhood_watch/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
