5461265ef0ab8a02748762c12184f09b19ebc768
Edith <you@example.com>
2018-10-21 03:07:08 +0300

    created class neighbourhood

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length = 100)
+    location = [
+        ('Nairobi', 'Nairobi'),
+        ('Kisumu', 'Kisumu'),
+        ('Mombasa', 'Mombasa'),
+        ('Eldoret', 'Eldoret'),
+    ]
+    local  = models.CharField(max_length=00, choices=location)
+    occupants = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
