4470f6311665694d62097785f083d3cef17b5d1f
Edith <you@example.com>
2018-10-21 23:48:09 +0300

    created Procfile and runtime files

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from .models import Neighbourhood,Business,Profile,Post,Comment,category,Location
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class HoodTestClass(TestCase):
+    pass
+    
