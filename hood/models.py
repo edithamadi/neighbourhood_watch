dc10f962e4a727981ee5720e3e83d8da22b1587e
Edith <you@example.com>
2018-10-21 04:59:03 +0300

    added class category and its functions

--- hood/models.py
+++ hood/models.py
@@ -139,4 +139,16 @@ class Location(models.Model):
 
     def delete_location(self):
         self.delete()
+
+class category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
     
