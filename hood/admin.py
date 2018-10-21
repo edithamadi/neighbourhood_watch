f365da316a86a4c53061290666cb8bd172788acf
Edith <you@example.com>
2018-10-21 23:57:14 +0300

    added models to admin page

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Neighbourhood,Business,Post,Profile,Comment,Location,category
 # Register your models here.
+
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Location)
+admin.site.register(category)
