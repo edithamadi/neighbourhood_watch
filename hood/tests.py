ce80c920eeaaf8afd28c08212aad09176ff09788
Edith <you@example.com>
2018-10-22 09:28:41 +0300

    extende navbar to welcome page

--- hood/tests.py
+++ hood/tests.py
@@ -3,6 +3,13 @@ from .models import Neighbourhood,Business,Profile,Post,Comment,category,Locatio
 from django.contrib.auth.models import User
 # Create your tests here.
 
-class HoodTestClass(TestCase):
-    pass
-    
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User(username='adriel',email='adriel@gmail.com')
+        self.new_user.save()
+        self.Nairobi = Neighbourhood(locale='nyati',location='Nairobi',occupants=5)
+        self.Nairobi.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Nairobi,Neighbourhood))
+        
