44e8a71791c15d399f7d5a99516c9ffbb5c90b4e
Edith <you@example.com>
2018-10-22 09:43:39 +0300

    run test for neighbourhood class

--- hood/tests.py
+++ hood/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
-from .models import Neighbourhood,Business,Profile,Post,Comment,category,Location
+from .models import Neighbourhood
 from django.contrib.auth.models import User
 # Create your tests here.
 
 class NeighbourhoodTestClass(TestCase):
     def setUp(self):
-        self.new_user = User(username='adriel',email='adriel@gmail.com')
+        self.new_user = User(id='1', username='adriel',email='adriel@gmail.com')
         self.new_user.save()
-        self.Nairobi = Neighbourhood(locale='nyati',location='Nairobi',occupants=5)
+        self.Nairobi = Neighbourhood(name='nyati',locale='Nairobi',occupants=5, user=self.new_user)
         self.Nairobi.save()
 
     def test_instance(self):
